14e1b3e309aad0b8e8318c06d3e94ad6513eda8d
galinagumenyuk <gymenyuk.galina@gmail.com>
2022-07-24 19:13:23 +0300

    h1

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -7,31 +7,33 @@ import s from "./Header.module.css";
 const Header = () => { 
     return (
         <div className={s.container}>
-            <div className={s.wrapper}>
-                <Svg/>
-                <p className={s.logo}>Finance<span className={cx(s.logo, s.logoActive)}>Ledger</span></p>
+            <div className={s.desktopWrapper}>
+                <div className={s.wrapper}>
+                    <Svg/>
+                    <p className={s.logo}>Finance<span className={cx(s.logo, s.logoActive)}>Ledger</span></p>
+                </div>
+                <nav>
+                    <ul className={ s.list}>
+                        <li className={s.items}>
+                            <a href="#..." className={s.link}>Home</a>
+                        </li>
+                        <li className={s.items}>
+                            <a href="#..." className={s.link}>About</a>
+                        </li>
+                        <li className={s.items}>
+                            <a href="#..." className={s.link}>Cases</a>
+                        </li>
+                        <li className={s.items}>
+                            <a href="#..." className={s.link}>Blog</a>
+                        </li>
+                        <li className={s.items}>
+                            <a href="#..." className={s.link}>Contact</a>
+                        </li>
+                    </ul>
+                </nav>
             </div>
-            <nav>
-                <ul className={ s.list}>
-                    <li className={s.items}>
-                        <a href="#..." className={s.link}>Home</a>
-                    </li>
-                    <li className={s.items}>
-                        <a href="#..." className={s.link}>About</a>
-                    </li>
-                    <li className={s.items}>
-                        <a href="#..." className={s.link}>Cases</a>
-                    </li>
-                    <li className={s.items}>
-                        <a href="#..." className={s.link}>Blog</a>
-                    </li>
-                    <li className={s.items}>
-                        <a href="#..." className={s.link}>Contact</a>
-                    </li>
-                </ul>
-            </nav>
-        </div>
-       
+            <h1 className={s.title}>The Sky Is The Limit</h1>
+        </div> 
     )
 }
 
