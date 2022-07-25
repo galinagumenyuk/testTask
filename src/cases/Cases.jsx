5d1a2b465799b811c28268fd1009af1eededc9e7
galinagumenyuk <gymenyuk.galina@gmail.com>
2022-07-25 21:52:42 +0300

    Modal

--- src/cases/Cases.jsx
+++ src/cases/Cases.jsx
@@ -1,12 +1,35 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import Modal from "./Modal";
 import s from "./Cases.module.css"
 
+
+const items = [
+    <img src={require("../images/cases/cases1.jpg")} alt="img1"  className={s.img} />,
+    <img src={require("../images/cases/cases2.jpg")} alt="img2" className={s.img} />,
+    <img src={require("../images/cases/cases3.jpg")} alt="img3" className={s.img} />,
+    <img src={require("../images/cases/cases4.jpg")} alt="img4" className={s.img} />,
+    <img src={require("../images/cases/cases5.jpg")} alt="img5" className={s.img} />,
+    <img src={require("../images/cases/cases6.jpg")} alt="img6"  className={s.img} />]
+
 const Cases = () => {
+    const [showModal, setShowModal] = useState(false);
+    const toggleModal = () => {
+    setShowModal((showModal) => !showModal);
+  };
+
     return (
         <div className={s.container}>
             <h3 className={s.subtitle}>This is what we do</h3>
             <h2 className={s.title}>Business Cases</h2>
             <p className={s.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto, sapiente!</p>
+            <ul className={s.list} onClick={toggleModal}>
+                    {items.map((item) => (
+                        <li key={items.indexOf(item)} >{item}</li>
+                    ))}
+            </ul>
+            {showModal && (
+        <Modal onClose={toggleModal} />
+      )} 
         </div>
     )
  };
---
+++ src/cases/Modal.jsx
@@ -0,0 +1,44 @@
+import { useEffect} from "react";
+import s from "./Modal.module.css";
+import "react-alice-carousel/lib/alice-carousel.css";
+import React from 'react';
+import AliceCarousel from 'react-alice-carousel';
+import 'react-alice-carousel/lib/alice-carousel.css';
+
+const handleDragStart = (e) => e.preventDefault();
+
+const items = [
+    <img src={require("../images/cases/cases1.jpg")} alt="img1"  onDragStart={handleDragStart} className={s.img}/>,
+    <img src={require("../images/cases/cases2.jpg")} alt="img2" onDragStart={handleDragStart} className={s.img}/>,
+    <img src={require("../images/cases/cases3.jpg")} alt="img3" onDragStart={handleDragStart} className={s.img}/>,
+    <img src={require("../images/cases/cases4.jpg")} alt="img4" onDragStart={handleDragStart} className={s.img}/>,
+    <img src={require("../images/cases/cases5.jpg")} alt="img5" onDragStart={handleDragStart} className={s.img}/>,
+    <img src={require("../images/cases/cases6.jpg")} alt="img6"  onDragStart={handleDragStart} className={s.img}/>]
+
+export default function ModalWindow({ onClose}) {
+
+  const handleBackdropClick = (e) => {
+    if (e.currentTarget === e.target) {
+    onClose();
+    }
+  };
+
+   const handleKeydown = (e) => {
+    if (e.code === "Escape") {
+    onClose();
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("keydown", handleKeydown);
+    return () => {window.removeEventListener("keydown",handleKeydown)}
+  })
+
+    return (
+        <div className={s.overlay} onClick={handleBackdropClick}>
+            <div className={s.modal}>
+           <AliceCarousel mouseTracking items={items} />
+        </div>
+      </div>
+    );
+}
