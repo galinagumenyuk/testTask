538f32c48f22e6c1138ec02a8338f0ffde7c5201
galinagumenyuk <gymenyuk.galina@gmail.com>
2022-07-26 18:28:12 +0300

    without overlay

--- src/components/team/Team.jsx
+++ src/components/team/Team.jsx
@@ -7,6 +7,23 @@ const Team = () => {
             <h3 className={s.subtitle}>Who we are</h3>
             <h2 className={s.title}>Our Professional Team</h2>
             <p className={s.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto, sapiente!</p>
+            <ul className={s.wrapper}>
+                <li>
+                    <img src={require("../../images/team/person1.jpg")} alt="person" className={s.img}></img>
+                    <p className={s.mainLabel}>John Doe</p>
+                    <p className={s.label}>President</p>
+                </li>
+                <li>
+                    <img src={require("../../images/team/person2.jpg")} alt="person" className={s.img}></img>
+                    <p className={s.mainLabel}>Jane Doe</p>
+                    <p className={s.label}>Vice  President</p>
+                </li>
+                <li>
+                    <img src={require("../../images/team/person3.jpg")} alt="person" className={s.img}></img>
+                    <p className={s.mainLabel}>Steve Smith</p>
+                    <p className={s.label}>Marketing Head</p>
+                </li>
+           </ul>
         </div>
     )
  };
