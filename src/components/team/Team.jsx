c7fdf8c26c9f58833628461e0df7813b6ee48490
galinagumenyuk <gymenyuk.galina@gmail.com>
2022-07-26 21:11:51 +0300

    svg

--- src/components/team/Team.jsx
+++ src/components/team/Team.jsx
@@ -1,5 +1,10 @@
 import react from "react";
 import s from "./Team.module.css";
+import {HandySvg} from 'handy-svg';
+import iconLinkedin from '../../images/svg/linked.svg';
+import iconFB from "../../images/svg/facebook.svg";
+import iconYoutube from "../../images/svg/youtube.svg";
+import iconTw from "../../images/svg/tw.svg";
 
 const Team = () => {
     return (
@@ -12,10 +17,42 @@ const Team = () => {
                     <div className={s.imgWrapper}>
                         <img src={require("../../images/team/person1.jpg")} alt="person" className={s.img}></img>
                         <div className={s.overlay}>
-                            <div className={s.overlayText}>hello</div>
+                            <ul className={s.overlayText}>
+                                <li><a href="#.">
+                                    <HandySvg
+                                        src={iconFB}
+                                        width="35"
+                                        height="35"
+                                        className={s.svgIcon}
+                                    />
+                                </a></li>
+                                <li><a href="#.">
+                                    <HandySvg
+                                        src={iconTw}
+                                        width="35"
+                                        height="35"
+                                        className={s.svgIcon}
+                                    />
+                                </a></li>
+                               <li><a href="#.">
+                                    <HandySvg
+                                        src={iconYoutube}
+                                        width="40"
+                                        height="35"
+                                        className={s.svgIcon}
+                                    />
+                                </a></li>
+                                <li><a href="#.">
+                                    <HandySvg
+                                        src={iconLinkedin}
+                                        width="31"
+                                        height="35"
+                                        className={s.svgIcon}
+                                    />
+                                </a></li>
+                            </ul>
                         </div>
                     </div>
-                    
                     <p className={s.mainLabel}>John Doe</p>
                     <p className={s.label}>President</p>
                 </li>
@@ -23,7 +60,40 @@ const Team = () => {
                   <div className={s.imgWrapper}>
                         <img src={require("../../images/team/person2.jpg")} alt="person" className={s.img}></img>
                         <div className={s.overlay}>
-                            <div className={s.overlayText}>hello</div>
+                            <ul className={s.overlayText}>
+                                <li><a href="#.">
+                                    <HandySvg
+                                        src={iconFB}
+                                        width="35"
+                                        height="35"
+                                        className={s.svgIcon}
+                                    />
+                                </a></li>
+                                <li><a href="#.">
+                                    <HandySvg
+                                        src={iconTw}
+                                        width="35"
+                                        height="35"
+                                        className={s.svgIcon}
+                                    />
+                                </a></li>
+                               <li><a href="#.">
+                                    <HandySvg
+                                        src={iconYoutube}
+                                        width="40"
+                                        height="35"
+                                        className={s.svgIcon}
+                                    />
+                                </a></li>
+                                <li><a href="#.">
+                                    <HandySvg
+                                        src={iconLinkedin}
+                                        width="31"
+                                        height="35"
+                                        className={s.svgIcon}
+                                    />
+                                </a></li>
+                            </ul>
                         </div>
                     </div>
                     <p className={s.mainLabel}>Jane Doe</p>
@@ -33,7 +103,40 @@ const Team = () => {
                     <div className={s.imgWrapper}>
                         <img src={require("../../images/team/person3.jpg")} alt="person" className={s.img}></img>
                         <div className={s.overlay}>
-                            <div className={s.overlayText}>hello</div>
+                            <ul className={s.overlayText}>
+                                <li><a href="#.">
+                                    <HandySvg
+                                        src={iconFB}
+                                        width="35"
+                                        height="35"
+                                        className={s.svgIcon}
+                                    />
+                                </a></li>
+                                <li><a href="#.">
+                                    <HandySvg
+                                        src={iconTw}
+                                        width="35"
+                                        height="35"
+                                        className={s.svgIcon}
+                                    />
+                                </a></li>
+                               <li><a href="#.">
+                                    <HandySvg
+                                        src={iconYoutube}
+                                        width="40"
+                                        height="35"
+                                        className={s.svgIcon}
+                                    />
+                                </a></li>
+                                <li><a href="#.">
+                                    <HandySvg
+                                        src={iconLinkedin}
+                                        width="31"
+                                        height="35"
+                                        className={s.svgIcon}
+                                    />
+                                </a></li>
+                            </ul>
                         </div>
                     </div>
                     <p className={s.mainLabel}>Steve Smith</p>
